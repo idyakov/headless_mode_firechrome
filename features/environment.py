30ee4d5483dafa717b5c6421c0a51388a48bdd50
Ilya Dyakov <dyak.ilya@gmail.com>
2024-05-30 17:54:56 -0700

    headless_mode_v1

---
+++ features/environment.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from app.application import Application
+from selenium.webdriver.chrome.options import Options
+
+
+def browser_init(context):
+    # driver_path = './chromedriver.exe'
+    # context.driver = webdriver.Chrome(executable_path=driver_path)
+    # #
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+
+    ### BROWSERS WITH DRIVERS: provide path to the driver file ###
+    # service = Service(executable_path='C:\Users\User\Downloads\geckodriver-0.34.0')
+    # context.driver = webdriver.Firefox(service=service)
+
+    ### SAFARI ###
+    # context.driver = webdriver.Safari()
+    """
+    :param context: Behave context
+    """
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(4)
+    context.app = Application(context.driver)
+    # Setup Chrome options for headless mode
+    ### HEADLESS MODE ####
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('--window-size=1920,1080')
+    service = Service(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(
+        options=options,
+        service=service
+    )
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    browser_init(context)
+
+
+def before_step(context, step):
+    print('\nStarted step: ', step)
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        print('\nStep failed: ', step)
+
+
+def after_scenario(context, feature):
+    context.driver.delete_all_cookies()
+    context.driver.quit()
